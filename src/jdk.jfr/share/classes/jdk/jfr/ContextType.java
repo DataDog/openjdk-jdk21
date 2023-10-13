e445799be16fc166f41b5259abb356e1c1bef3eb
Jaroslav Bachorik <j.bachorik@gmail.com>
2023-10-13 10:49:50 +0200

    Remove the captured context concept

--- src/jdk.jfr/share/classes/jdk/jfr/ContextType.java
+++ src/jdk.jfr/share/classes/jdk/jfr/ContextType.java
@@ -11,40 +11,5 @@ public abstract class ContextType extends BaseContextType {
         Stream<Class<? extends ContextType>> types();
     }
 
-    public static final class Captured<T extends ContextType.Capturable<T>> implements Closeable, AutoCloseable {
-        private final T parent, current;
-
-        Captured(T parent, T current) {
-            assert parent != null;
-            assert current != null;
-            this.parent = parent;
-            this.current = current;
-        }
-
-        @Override
-        public void close() {
-            parent.set();
-        }
-
-        public T get() {
-            return current;
-        }
-
-        public Captured<T> capture() {
-            return current.capture();
-        }
-    }
-
-    public static abstract class Capturable<T extends Capturable<T>> extends ContextType {
-        public Capturable() {}
-
-        @SuppressWarnings("unchecked")
-        public final Captured<T> capture() {
-            return new Captured<T>((T) this, snapshot());
-        }
-
-        abstract protected T snapshot();
-    }
-
     protected ContextType() {}
 }
